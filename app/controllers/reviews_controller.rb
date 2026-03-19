9c7106b22a2b4a8b6638e08724df3246a0cafd41
IkuroMaira <gwen.bussac@protonmail.com>
2026-03-19 17:49:49 +0100

    wip

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,5 +2,8 @@ class ReviewsController < ApplicationController
   def show
     @deck = Deck.find(params[deck_id])
     @cards = @deck.cards
+    # position = params[position]
+    @card = @deck.cards.where(card_id: position)
+    @position =
   end
 end
