7350103b229f79307e2ce6ca9db752ae74f86a29
IkuroMaira <gwen.bussac@protonmail.com>
2026-04-01 20:04:34 +0200

    WIP

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,8 +4,8 @@ class ReviewsController < ApplicationController
     @cards = @deck.cards
     position = (params[:position] || 0).to_i
     # Dans Rails, params est un hash (l'équivalent d'un objet JS), et ses clés sont des symboles.
-    # Donc pour accéder à la valeur, on doit utiliser la bonne clé — :position et pas position (qui serait une variable).
-    @card = @cards[position]
-    @position = position
+    # Donc pour accéder à la valeur, on doit utiliser la bonne clé — `:position` et pas `position` (qui serait une variable).
+    @position = position.clamp(0, @cards.count - 1)
+    @card = @cards[@position]
   end
 end
